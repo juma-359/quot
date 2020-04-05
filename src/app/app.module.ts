1b86afa4fea7235094dc4ae06c6e51289f496298
juma-359 <joeljuma08@gmail.com>
2020-04-05 21:02:43 +0300

    new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VotesComponent } from './votes/votes.component';
+import { CountInputComponent } from './count-input/count-input.component';
+import { QuoteComponent } from './quote/quote.component';
+import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { DateCountPipesComponent } from './date-count-pipes/date-count-pipes.component';
+import { DateCountPipe } from './date-count.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VotesComponent,
+    CountInputComponent,
+    QuoteComponent,
+    QuoteDetailComponent,
+    QuoteFormComponent,
+    DateCountPipesComponent,
+    DateCountPipe
   ],
   imports: [
     BrowserModule,
